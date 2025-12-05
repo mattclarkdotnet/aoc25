855468b6858d42432a41763587f8547dfc8da7e5
matt <matt@mattclark.net>
2025-12-05 08:35:33 +0800

    some tidying of day 4

--- 4/part2.py
+++ 4/part2.py
@@ -3,7 +3,7 @@
 
 import itertools
 from sys import argv
-from typing import Dict, Iterable, Tuple
+from typing import Dict, Iterable
 
 # Define a reasonable type for the input, which will depend a lot on the problem
 
@@ -14,18 +14,13 @@ input: InputMap = {}
 # Parse the input, stripping newlines and skipping blank lines
 #
 row = 0
-width = 0
 with open(argv[1]) as infile:
     for line in infile.readlines():
         line = line.strip()
         if len(line) > 0:
-            width = max(len(line), width)
             for col, char in enumerate(line):
                 input[(col, row)] = True if char == "@" else False
             row += 1
-height = row
-
-print(f"w:{width}, h:{height}")
 
 # If needed, do some extra preprocessing, e.g. to create helper structures
 pass
@@ -36,6 +31,26 @@ pass
 adjacencies = list(itertools.product([-1, 0, 1], [-1, 0, 1]))
 adjacencies.remove((0, 0))
 assert len(adjacencies) == 8
+
+
+def dim(m: InputMap) -> tuple[int, int]:
+    w = 0
+    h = 0
+    for p in m.keys():
+        w = max(w, p[0])
+        h = max(h, p[1])
+    return (w + 1, h + 1)
+
+
+def printablemap(m: InputMap) -> str:
+    (w, h) = dim(m)
+    rows = []
+    for y in range(h):
+        r: str = ""
+        for x in range(w):
+            r += "@" if m[(x, y)] is True else "."
+        rows.append(r)
+    return "\n".join(rows)
 
 
 def neighbouring(input: InputMap, position: Position) -> Iterable[Position]:
@@ -49,14 +64,17 @@ def neighbour_vals(input: InputMap, position: Position) -> Iterable[bool]:
 
 
 def accessible(input: InputMap) -> Iterable[Position]:
-    for x in range(width):
-        for y in range(height):
+    w, h = dim(input)
+    for x in range(w):
+        for y in range(h):
             if input[(x, y)] is True:
                 rolls = sum(1 for i in neighbour_vals(input, (x, y)) if i is True)
                 if rolls < 4:
                     yield (x, y)
 
 
+# Basic testing
+#
 if argv[1] == "input":
     assert input[(0, 2)] is True
     assert input[(1, 1)] is True
@@ -67,6 +85,11 @@ if argv[1] == "input":
     assert input[(2, 0)] is False
     assert input[(3, 4)] is False
     assert input[(0, 3)] is False
+
+# Do the work
+#
+
+print(printablemap(input))
 
 removed = 0
 while True:
